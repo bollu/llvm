8781e9d6d0955296efdb61bc557b53b940a78a29
Siddharth Bhat <siddu.druid@gmail.com>
2017-09-17 12:57:21 +0200

    [PassManager] teach legacy and newPM to send info about eqsat

--- include/llvm/Transforms/GraphRewrite/GraphRewrite.h
+++ include/llvm/Transforms/GraphRewrite/GraphRewrite.h
@@ -22,15 +22,71 @@
 
 namespace llvm {
 
+class PEGBasicBlock;
+class PEGInst;
+class PEGFunction;
+class PEGOperand;
+class PEGOperand {
+
+};
+
+class PEGInst : public User, public ilist_node_with_parent<PEGInst, PEGBasicBlock,
+                                    ilist_sentinel_tracking<true>> {
+  PEGBasicBlock *Parent = nullptr;  // Pointer to the owning basic block.
+  std::vector<PEGOperand *> Operands;
+};
+
+
+// Structured very similar to machineBB;
+class PEGBasicBlock : public ilist_node_with_parent<PEGBasicBlock, PEGFunction> {
+  using PEGInsts = ilist<PEGInst, ilist_sentinel_tracking<true>>;
+  PEGInsts Insts;
+  const BasicBlock *BB;
+  std::vector<PEGBasicBlock *> Predecessors;
+  std::vector<PEGBasicBlock *> Successors;
+
+  // Intrusive list support
+  PEGBasicBlock() = default;
+
+  explicit PEGBasicBlock(PEGFunction &PEGF, const BasicBlock *BB);
+
+  ~PEGBasicBlock();
+
+  // MachineBasicBlocks are allocated and owned by MachineFunction.
+  friend class PEGFunction;
+};
+
+class PEGFunction {
+  const Function *Fn;
+
+  // List of machine basic blocks in function
+  using BasicBlockListType = ilist<PEGBasicBlock>;
+  BasicBlockListType BasicBlocks;
+
+  std::vector<PEGBasicBlock*> MBBNumbering;
+
+
+public:
+  PEGFunction(const Function *Fn);
+  PEGFunction(const PEGFunction &) = delete;
+  PEGFunction &operator=(const PEGFunction &) = delete;
+  ~PEGFunction();
+
+  /// getFunction - Return the LLVM function that this machine code represents
+  const Function *getFunction() const { return Fn; }
+
+  /// getName - Return the name of the corresponding LLVM function.
+  StringRef getName() const;
+
+
+};
+
 class GraphRewritePass : public PassInfoMixin<GraphRewritePass> {
 
 public:
   static StringRef name() { return "GraphRewritePass"; }
-
-  explicit GraphRewritePass()
-  {}
-
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) { return llvm::PreservedAnalyses::all(); }
+  explicit GraphRewritePass() {}
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
 };
 
 /// \brief The legacy pass manager's instcombine pass.
@@ -47,8 +103,8 @@ public:
     initializeGraphRewriteLegacyPassPass(*PassRegistry::getPassRegistry());
   }
 
-  void getAnalysisUsage(AnalysisUsage &AU) const override {};
-  bool runOnFunction(Function &F) override { return false; }
+  void getAnalysisUsage(AnalysisUsage &AU) const override;
+  bool runOnFunction(Function &F) override;
 };
 
 Pass *createGraphRewriteLegacyPass();
--- lib/Transforms/GraphRewrite/GraphRewrite.cpp
+++ lib/Transforms/GraphRewrite/GraphRewrite.cpp
@@ -10,9 +10,14 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Analysis/CFG.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/Analysis/ScalarEvolution.h"
+
 #include "llvm/Analysis/ConstantFolding.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Transforms/GraphRewrite/GraphRewrite.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Utils/Local.h"
@@ -23,6 +28,50 @@
 #define DEBUG_TYPE "graphrewrite"
 using namespace llvm;
 
+//===----------------------------------------------------------------------===//
+// GraphRewrite
+//===----------------------------------------------------------------------===//
+
+
+// Pass code
+// Modeled after EarlyCSE.
+class GraphRewrite {
+public:
+      GraphRewrite(DominatorTree &DT,LoopInfo &LI, ScalarEvolution &SE)
+      : DT(DT), LI(LI), SE(SE) {
+  }
+
+  bool run(Function &F);
+private:
+    DominatorTree &DT;
+    LoopInfo &LI;
+    ScalarEvolution &SE;
+};
+
+bool GraphRewrite::run(Function &F) {
+  return false;   
+}
+//===----------------------------------------------------------------------===//
+// GraphRewritePass
+//===----------------------------------------------------------------------===//
+
+PreservedAnalyses llvm::GraphRewritePass::run(Function &F, FunctionAnalysisManager &AM) {
+  auto &LI = AM.getResult<LoopAnalysis>(F);
+  auto &DT = AM.getResult<DominatorTreeAnalysis>(F);
+  auto &SE = AM.getResult<ScalarEvolutionAnalysis>(F);
+  GraphRewrite GR(DT, LI, SE);
+  GR.run(F);
+
+
+  PreservedAnalyses PA = PreservedAnalyses::none();
+  return PA;
+
+} // { return llvm::PreservedAnalyses::all(); }
+
+
+//===----------------------------------------------------------------------===//
+// GraphRewriteLegacyPass
+//===----------------------------------------------------------------------===//
 char GraphRewriteLegacyPass::ID = 0;
 
 INITIALIZE_PASS_BEGIN(GraphRewriteLegacyPass, "graphrewrite",
@@ -37,5 +86,22 @@ Pass *llvm::createGraphRewriteLegacyPass() {
 
 void llvm::initializeGraphRewrite(PassRegistry &Registry) {
     initializeGraphRewriteLegacyPassPass(Registry);
+}
+
+void GraphRewriteLegacyPass::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.setPreservesCFG();
+  AU.addRequired<DominatorTreeWrapperPass>();
+
+};
+
+
+bool GraphRewriteLegacyPass::runOnFunction(Function &F) { 
+    auto &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+    auto &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
+    auto &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+
+    GraphRewrite GR(DT, LI, SE);
+    return GR.run(F);
+
 }
 
